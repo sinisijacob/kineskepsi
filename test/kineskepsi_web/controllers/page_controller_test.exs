abc17b5cbccdb07d06eb76e5bf104e222e7ea6a4
jacob <sinisijacob@gmail.com>
2024-06-08 10:12:28 -0400

    a rename and adding gitignore

---
+++ test/kineskepsi_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule KineskepsiWeb.PageControllerTest do
+  use KineskepsiWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
