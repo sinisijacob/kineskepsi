534bef37d98159f9d7d082415cb4820b7a3ac6a1
jacob <sinisijacob@gmail.com>
2024-07-01 20:06:58 -0400

    single header, deletion of default home page

--- lib/kineskepsi_web/controllers/page_controller.ex
+++ lib/kineskepsi_web/controllers/page_controller.ex
@@ -2,9 +2,7 @@ defmodule KineskepsiWeb.PageController do
   use KineskepsiWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
+    # Redirect to posts from home
     redirect(conn, to: ~p"/posts")
-    # render(conn, :home, layout: false)
   end
 end
